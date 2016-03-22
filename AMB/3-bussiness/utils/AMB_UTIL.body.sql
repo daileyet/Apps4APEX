a935e0cb63af98ae1a6e5185faffb1b708eb0ab5
dailey.yet <dailey.yet@outlook.com>
2016-03-22 23:19:37 +0800

    update export download

--- AMB/3-bussiness/utils/AMB_UTIL.body.sql
+++ AMB/3-bussiness/utils/AMB_UTIL.body.sql
@@ -10,16 +10,22 @@ end generate_guid;
 
 
 
-procedure print_clob(p_clob CLOB)
+procedure print_clob(p_clob CLOB,p_style varchar2 default 'HTP')
   AS  
-    v_clob_length number:=1000;  
+    v_clob_length number:=DBMS_LOB.GETLENGTH(p_clob);  
     v_offset number:=1;  
   BEGIN  
      
     WHILE v_offset < v_clob_length  
     LOOP  
-      htp.p(DBMS_LOB.SUBSTR(p_clob,32767,v_offset));  
+    IF p_style = 'console' then
+      DBMS_OUTPUT.PUT_LINE(DBMS_LOB.SUBSTR(p_clob,4000,v_offset));
+      v_offset:=v_offset+4000;  
+    else
+      htp.p(DBMS_LOB.SUBSTR(p_clob,32767,v_offset)); 
       v_offset:=v_offset+32767;  
+    end if;
+      
     END LOOP;  
 END;
 
@@ -30,6 +36,19 @@ begin
 	htp.p('Cache-Control: no-cache');
 	htp.p('Pragma: no-cache');
 	owa_util.http_header_close;
+end;
+
+
+procedure download_file(p_mine varchar2,p_file_clob CLOB,p_filename varchar2)
+as
+	v_length INTEGER;
+begin
+	v_length:=DBMS_LOB.GETLENGTH(p_file_clob); 
+	owa_util.mime_header( nvl(p_mine,'application/octet'), FALSE );
+	htp.p('Content-length: ' || v_length);
+	htp.p('Content-Disposition: attachment; filename="'||p_filename||'"');
+	owa_util.http_header_close;
+	print_clob(p_file_clob);
 end;
 
 end AMB_UTIL;
--- AMB/3-bussiness/utils/AMB_UTIL.sql
+++ AMB/3-bussiness/utils/AMB_UTIL.sql
@@ -13,11 +13,13 @@ as
 function generate_guid return varchar2;
 
 
-procedure print_clob(p_clob CLOB);
+procedure print_clob(p_clob CLOB,p_style varchar2 default 'HTP');
 
 /**
  * when start a aemand process by requested by ajax, response header setting.
  */
 procedure set_ajax_header(p_response_type varchar2 default 'text/json');
+
+procedure download_file(p_mine varchar2,p_file_clob CLOB,p_filename varchar2);
 
 end AMB_UTIL;
